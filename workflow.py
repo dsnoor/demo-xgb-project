b04e59ef10605b2898feef6793dcf1ce7b137392
Yasha Borstein <yashab@iguazio.com>
2020-03-22 09:48:47 +0000

    refactored mlrun/functions repo

--- workflow.py
+++ workflow.py
@@ -1,51 +1,57 @@
 from kfp import dsl
 from mlrun import mount_v3io
 
+DATASET = "iris"
+LABELS  = "labels"
+MODEL_KEY = "models"
+
+import os
+# path is created automatically if it doesn"t exists
+DATA_PATH = "/User/demo-xgb-project/artifacts"
+os.makedirs(DATA_PATH, exist_ok=True)
+
 funcs = {}
 
 def init_functions(functions: dict, params=None, secrets=None):
     for f in functions.values():
         f.apply(mount_v3io())
 
-import os
-ARTIFACT_PATH = '/User/demo-xgb-project/artifacts'
-os.makedirs(ARTIFACT_PATH, exist_ok=True)
-
-        
 @dsl.pipeline(
-    name='My XGBoost training pipeline',
-    description='Shows how to use mlrun.'
+    name="My XGBoost training pipeline",
+    description="Shows how to use mlrun."
 )
 def kfpipeline():
-    ingest = funcs['get_toy_data'].as_step(
-        name='acquire',
-        params={'dataset': 'iris'},
-        outputs=['data'],
-        out_path=ARTIFACT_PATH)
+    ingest = funcs["get_data"].as_step(
+        name="get data",
+        params={"dataset": DATASET},
+        outputs=[DATASET],
+        out_path=DATA_PATH)
 
-    describe = funcs['describe'].as_step(
-        name='describe',
-         params={"key": "summary", "label_column": "labels"},
-        inputs={"table": ingest.outputs['data']},
-        outputs=['data'],
-        out_path=ARTIFACT_PATH)
+    describe = funcs["summary"].as_step(
+        name="summary",
+        params={"key": "summary", "label_column": LABELS},
+        inputs={"table": ingest.outputs[DATASET]},
+        outputs=[DATASET])
     
-    configs = funcs['get_model_config'].as_step(
-        name='config',
-        params={'config': 'xgboost-conf.json'},
-        outputs=['class_params', 'fit_params'],
-        out_path=ARTIFACT_PATH)
-    
-    train = funcs['train_model'].as_step(
-        name='train',
-        # hyperparams={'eta': eta, 'gamma': gamma},
-        # selector='max.accuracy',
-        params={'sample': -1, 'label_column': 'labels'}
-        inputs={'data_key'    : ingest.outputs['data'],
-                'class_params': configs.outputs['class_params'],
-                'fit_params'  : configs.outputs['fit_params']},
-        outputs=['model'],
-        out_path=ARTIFACT_PATH)
+    train = funcs["train"].as_step(
+        name="train",
+        params={"model_pkg_class" : "sklearn.linear_model.LogisticRegression",
+                "model_key"       : MODEL_KEY, 
+                "sample"          : -1, 
+                "label_column"    : LABELS,
+                "test_size"       : 0.10,
+                "class_params_updates"  : {"random_state": 1},
+                "fit_params_updates"    : {}},
+        inputs={"data_key"        : ingest.outputs[DATASET]},
+        outputs=[MODEL_KEY, "test_set"])
 
-    # deploy the trained model using a nuclio real-time function
-    deploy = funcs['serving'].deploy_step(models={'iris_v1': train.outputs['model']})
+    test = funcs["test"].as_step(
+        name="test",
+        params={"label_column": LABELS},
+        inputs={"models_dir"  : train.outputs[MODEL_KEY],
+                "test_set"    : train.outputs["test_set"]},
+        outputs=[MODEL_KEY])
+
+    deploy = funcs["server"].deploy_step(
+       project=DATASET, 
+       models={f"{DATASET}_v1": train.outputs[MODEL_KEY]})
